a9291de4c0e25f8e7874794bc38221a3beb6f9ed
Diego Cuevas <diegocuevas579@gmail.com>
2019-10-22 17:16:46 -0500

    Add models and CRUD in user controller

---
+++ app/controllers/api/users_controller.rb
@@ -0,0 +1,30 @@
+class Api::UsersController < ApplicationController
+  
+  def index
+    render json: User.all
+  end
+
+  def show
+    render json: User.find(params[:id])
+  end
+
+  def create
+    render json: User.create(user_params)
+  end
+
+  def update
+    render json: User.find(params[:id].update(user_params))
+  end
+
+  def delete
+    article = Article.find_by(id: params[:id])
+    article.destroy
+    render :status => :no_content
+  end
+
+  private 
+  
+  def user_params
+    params.permit(:name, :last_name, :age, :role)
+  end
+end
---
+++ app/models/register.rb
@@ -0,0 +1,3 @@
+class Register < ApplicationRecord
+  belongs_to :user
+end
